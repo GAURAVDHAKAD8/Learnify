0eb122f15028e95725bd3b7512625ea9f086d986
Gaurav Dhakad <gdgaurav856@gmail.com>
2025-11-02 16:07:48 +0530

    first commit

---
+++ src/components/student/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <title>hero</title>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/pages/student/MyEnrollments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyEnrollments = () => {
+  return (
+    <div>
+      myenrollments
+    </div>
+  )
+}
+
+export default MyEnrollments
